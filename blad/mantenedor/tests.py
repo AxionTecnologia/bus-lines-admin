3d0b2d8a8f6dd7e7549904b8f3b5658b96645299
Milton Inostroza <minostro@minostro.com>
2014-05-24 19:33:26 -0500

    adding test to user creation, and validation at username level

--- blad/mantenedor/tests.py
+++ blad/mantenedor/tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
 
 from mantenedor.models import AuthUser, Line, AppUser
 
@@ -63,7 +64,7 @@ class LineTestCase(TestCase):
     self.assertEqual(u"Línea 6", line.__unicode__())
 
 
-class AdminMainPageTestCase(TestCase):
+class AdminMainViewTestCase(TestCase):
 
   def setUp(self):
     user = AuthUser.objects.create_user(username="me", password="me")
@@ -73,7 +74,43 @@ class AdminMainPageTestCase(TestCase):
   def test_superuser_can_only_have_access_to_mantenedor_and_auth(self):
     client = Client()
     client.login(username="me", password="me")
-    response = client.get("/admin/")
+    response = client.get(reverse("admin:index"))
     app_list = response.context['app_list']
     self.assertEqual(["Auth", "Mantenedor"], map(lambda app: app['name'], app_list))
+
+
+class AppUserViewTestCase(TestCase):
+
+  def setUp(self):
+    user = AuthUser.objects.create_user(username="me", password="me")
+    user.is_superuser = user.is_staff = user.is_active = True
+    user.save()
+
+  def test_app_user_creation_add_auth_user_as_part_of_app_user(self):
+    line = Line.objects.create(number=5)
+    client = Client()
+    client.login(username="me", password="me")
+    response = client.post(reverse("admin:mantenedor_appuser_add"), {'line': line.id, 'username': "minostro"})
+    self.assertRedirects(response, reverse("admin:mantenedor_appuser_changelist"))
+    self.assertEqual(1, AppUser.objects.all().count())
+    self.assertEqual(1, AuthUser.objects.filter(username="minostro").count())
+
+
+  def test_it_is_not_possible_to_create_two_repeated_users(self):
+    line = Line.objects.create(number=5)
+    client = Client()
+    client.login(username="me", password="me")
+
+    response = client.post(reverse("admin:mantenedor_appuser_add"), {'line': line.id, 'username': "minostro"})
+    self.assertRedirects(response, reverse("admin:mantenedor_appuser_changelist"))
+
+    response = client.post(reverse("admin:mantenedor_appuser_add"), {'line': line.id, 'username': "minostro"})
+    #NOTE: I couldn't use assertFormError(response, 'form'..), because AppUserForm is not in the context map.
+    # It seems assertFormError was meant to work with personalized views and not with Django Admin views
+    # response.context['adminform'].form.errors does the trick, but I prefered to use contains.
+    self.assertContains(response, "User name already exists.", status_code=200)
+
+    self.assertEqual(1, AppUser.objects.all().count())
+    self.assertEqual(1, AuthUser.objects.filter(username="minostro").count())
+
 
